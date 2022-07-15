1f149b642f7904ab52596258ed9de8024e9f0fc0
Zuckuu <entbit12@gmail.com>
2022-07-14 22:23:43 -0500

    hero section done

---
+++ src/component/Hero.js
@@ -0,0 +1,12 @@
+import "../App.css";
+import grip from "../asset/airbnb-photo.png";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img className="hero--photo" src={grip} alt="" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  );
+}
