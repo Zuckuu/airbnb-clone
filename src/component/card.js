b74a10ad0e3e43067178628b05e2b26cc79a2f42
Zuckuu <entbit12@gmail.com>
2022-07-17 14:12:20 -0500

    online stuff

--- src/component/card.js
+++ src/component/card.js
@@ -2,14 +2,24 @@ import "../App.css";
 //import star from "../asset/red-star.png"
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <section className="card">
-      {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={props.img} className="card--image" alt="" />
       <div className="card--stats">
         <img src={props.star} className="starPic" alt="" />
         <p className="ratingText">
-          {props.rating} <span className="greyText">({props.reviewCount}) • {props.location}</span>
+          {props.rating}{" "}
+          <span className="greyText">
+            ({props.reviewCount}) • {props.location}
+          </span>
         </p>
       </div>
       <div className="cardDesContainer">
