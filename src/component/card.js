27235d62e9e6a5c89e618e315b9737bfc96203b3
Zuckuu <entbit12@gmail.com>
2022-07-16 20:27:17 -0500

    adding card text

--- src/component/card.js
+++ src/component/card.js
@@ -1,10 +1,21 @@
 import "../App.css";
+import katie from "../asset/Katie-Zaferes-pic.jpg"
+import star from "../asset/red-star.png"
 
 
 export default function Card() {
     return (
-      <section>
-          <h1>hello</h1>
+      <section className="card">
+          <img src={katie} className="picture" alt="" />
+          <div className="rating">
+            <img src={star} alt="" /> 5.0 <span className="greyText">(6) * USA</span>
+          </div>
+          <div>
+            <p>Life lessons with Katie Zaferes</p>
+          </div>
+          <div>
+            <p> <span className="boldText">From $136</span> / person</p>
+          </div>
       </section>
     );
   }
